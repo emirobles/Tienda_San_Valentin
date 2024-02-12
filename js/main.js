c95f417d36a5c0bb0807d89eab6b616e88873272
Emilce_Robles <emi.nrobles@gmail.com>
2024-02-12 18:29:54 -0300

    Avances HTML y CSS

---
+++ js/main.js
@@ -0,0 +1,104 @@
+//Products
+const products = [
+    // Flowers
+    {
+        id: "flower-01",
+        title: "Ramo de Flores",
+        imagen: "./img/flowers/flower1.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-02",
+        title: "Ramo de Flores2",
+        imagen: "./img/flowers/flower2.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-03",
+        title: "Ramo de Flores3",
+        imagen: "./img/flowers/flower3.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-04",
+        title: "Ramo de Flores4",
+        imagen: "./img/flowers/flower4.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-05",
+        title: "Ramo de Flores5",
+        imagen: "./img/flowers/flower5.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-06",
+        title: "Ramo de Flores6",
+        imagen: "./img/flowers/flower6.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-07",
+        title: "Ramo de Flores7",
+        imagen: "./img/flowers/flower7.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-08",
+        title: "Ramo de Flores8",
+        imagen: "./img/flowers/flower8.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-09",
+        title: "Ramo de Flores9",
+        imagen: "./img/flowers/flower9.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },
+    {
+        id: "flower-10",
+        title: "Ramo de Flores10",
+        imagen: "./img/flowers/flower10.png",
+        categoria: {
+            nombre: "Flowers",
+            id: "flowers"
+        },
+        precio: 3000
+    },   
+]
